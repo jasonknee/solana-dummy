1784b7a397004c22f2a260bf6b17b44bc257f24d
Jason Nghe <jnghe@belong.gg>
2021-09-23 10:01:06 -0700

    feat: v2 of app

--- programs/solana-dummy/src/lib.rs
+++ programs/solana-dummy/src/lib.rs
@@ -4,40 +4,42 @@ declare_id!("Cgt34BPwrxsLp6AExpDRzbbTZS9wmbTuNUvWV1fqWNFi");
 
 #[program]
 pub mod solana_dummy {
-    use super::*;
+  use super::*;
 
-    pub fn create(ctx: Context<Create>) -> ProgramResult {
-        let base_account = &mut ctx.accounts.base_account;
-        base_account.count = 0;
-        Ok(())
-    }
+  pub fn initialize(ctx: Context<Initialize>, data: String) -> ProgramResult {
+      let base_account = &mut ctx.accounts.base_account;
+      let copy = data.clone();
+      base_account.data = data;
+      base_account.data_list.push(copy);
+      Ok(())
+  }
 
-    pub fn increment(ctx: Context<Increment>) -> ProgramResult {
-        let base_account = &mut ctx.accounts.base_account;
-        base_account.count += 1;
-        Ok(())
-    }
+  pub fn update(ctx: Context<Update>, data: String) -> ProgramResult {
+      let base_account = &mut ctx.accounts.base_account;
+      let copy = data.clone();
+      base_account.data = data;
+      base_account.data_list.push(copy);
+      Ok(())
+  }
 }
 
-// Transaction instructions
 #[derive(Accounts)]
-pub struct Create<'info> {
-    #[account(init, payer = user, space = 16 + 16)]
-    pub base_account: Account<'info, BaseAccount>,
-    #[account(mut)]
-    pub user: Signer<'info>,
-    pub system_program: Program <'info, System>,
+pub struct Initialize<'info> {
+  #[account(init, payer = user, space = 64 + 64)]
+  pub base_account: Account<'info, BaseAccount>,
+  #[account(mut)]
+  pub user: Signer<'info>,
+  pub system_program: Program<'info, System>,
 }
 
-// Transaction instructions
 #[derive(Accounts)]
-pub struct Increment<'info> {
-    #[account(mut)]
-    pub base_account: Account<'info, BaseAccount>,
+pub struct Update<'info> {
+  #[account(mut)]
+  pub base_account: Account<'info, BaseAccount>,
 }
 
-// An account that goes inside a transaction instruction
 #[account]
 pub struct BaseAccount {
-    pub count: u64,
+  pub data: String,
+  pub data_list: Vec<String>,
 }
